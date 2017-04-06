ca8cde105f0ad019d4980d41f7f76f40edb1b4bb
mohmedbm <mohmedbamhrz@gmail.com>
2017-04-05 23:33:18 -0700

    First commit

---
+++ Class 10/exersie.js
@@ -0,0 +1,15 @@
+
+//chat widget
+// text field
+//ask them for their name first(prompt)
+
+//submit button next to the field
+
+
+//every text you added to the
+
+//text area
+
+
+///jacob: hello
+// jacob : how are you
---
+++ Class 10/review.js
@@ -0,0 +1,30 @@
+//review
+
+//jquery
+//html
+//to use the jquery on html you need too add jquery library
+//you can downlaod it from jquery website or you can use cdn
+//CDN -->contant delivery network
+// $();  // ---> to check if thetre is jquery on the page
+// <html>
+//
+//   <form>
+//     <input id = "firstName" type = "text">
+//     <input id = "submit" type = "submit">
+//   </form>
+//
+// ///how to select elements in jquery
+// $("#firstName");
+// $("#submit");
+// $('from').children()
+//$('form')firstChild
+// $('input[type = text]')
+//
+// </html>
+
+$('#submit').submit(function(event){
+  event.preventDefault();
+})
+
+
+//firebase - ??????????
